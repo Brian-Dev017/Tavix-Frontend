b65f4bcebb70520fd1f1c54f1d64fa2e9963531b
Brian_Dev017 <bama.17.ma@gmail.com>
2026-06-18 19:44:07 -0500

    chore: remove loading message from RouterView

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,5 @@ import Toast from "primevue/toast";
   <Toast />
   <RouterView v-slot="{ Component }">
     <component :is="Component" v-if="Component" />
-    <div v-else style="padding: 2rem; font-family: sans-serif; color: #333">
-      <h2>Cargando…</h2>
-      <p>Si esto persiste, verifica la consola del navegador (F12).</p>
-    </div>
   </RouterView>
 </template>
